26c5fde11490b415db401f2e561927a6f3d44536
Halothus <60372646+Halothus@users.noreply.github.com>
2020-08-10 15:22:11 -0300

    created Navbar for ecommerce
    Added tailwind to style created navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido Andres 
         </p>
         <a
           className="App-link"
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          huehuehue
         </a>
       </header>
     </div>
