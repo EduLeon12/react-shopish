70115f0086376a1b86adc7ed8290fd7d932974d6
Halothus <60372646+Halothus@users.noreply.github.com>
2020-08-17 13:29:50 -0300

    Added Cursor pointer

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,9 @@ function NavBar() {
     <nav id="header" className="w-full z-30 top-0 py-1">
       <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3">
         <div className="order-1 md:order-2">
-          <span className="flex items-center tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl ">
+          <span className="flex items-center tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl cursor-pointer ">
             <svg
-              className="fill-current text-gray-800 mr-2"
+              className="fill-current text-gray-800 mr-2 "
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
@@ -20,7 +20,7 @@ function NavBar() {
           </span>
         </div>
         <div className="order-2 md:order-3 flex items-center" id="nav-content">
-          <span className="inline-block no-underline hover:text-black">
+          <span className="inline-block no-underline hover:text-black cursor-pointer">
             <svg
               className="fill-current hover:text-black"
               xmlns="http://www.w3.org/2000/svg"
@@ -38,9 +38,9 @@ function NavBar() {
               <path d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z"></path>
             </svg>
           </span>
-          <span className="pl-3 inline-block no-underline hover:text-black">
-         <CartIcon />
-         </span>
+          <span className="pl-3 inline-block no-underline hover:text-black cursor-pointer ">
+            <CartIcon />
+          </span>
         </div>
       </div>
     </nav>
